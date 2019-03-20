798a41396b184821c8c58f569dfc7c0fa3cb62f9
Evan Lane <evanklane@yahoo.com>
2019-03-20 15:32:00 -0400

    added cloud function for notification + some style

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,40 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase)
 
-
-
-exports.helloWord = functions.https.onRequest((req,res)=>{
+exports.helloWorld = functions.https.onRequest((req,res)=>{
   res.send('hello FSB world')
 })
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+const createNotification = (notification => {
+  return admin.firestore().collection('notifications')
+  .add(notification)
+  .then(doc => console.log('notification added', doc))
+})
+
+exports.projectCreated = functions.firestore
+  .document('projects/{projectId}')
+  .onCreate(doc => {
+    const project = doc.data();
+    const notification = {
+      content: 'Added a new project',
+      user: `${project.authorFirstName} ${project.authorLastName}`,
+      time: admin.firestore.FieldValue.serverTimestamp()
+    }
+    return createNotification(notification);
+});
+// create trigger when a user has been created
+exports.userJoined = functions.auth.user()
+  .onCreate(user => {
+
+    return admin.firestore().collection('users')
+      .doc(user.uid).get().then(doc => {
+        const newUser = doc.data()
+        const notification = {
+          content: 'Joined the party!',
+          user: `${newUser.firstName} ${newUser.lastName}`,
+          time: admin.firestore.FieldValue.serverTimestamp()
+        }
+      return createNotification(notification);
+      })
+  })
